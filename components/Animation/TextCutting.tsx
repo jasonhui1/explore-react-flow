ca8f8ae6f9219ef303fd4282f029bae6503dea9c
jasonhui1 <jason903813@gmail.com>
2023-04-11 01:42:49 +0800

    Add text cutting node

---
+++ components/Animation/TextCutting.tsx
@@ -0,0 +1,82 @@
+import { Box, Button, Card, Divider, Flex, Heading, ListItem, Text, UnorderedList } from '@chakra-ui/react';
+import NodeWrapper from '../Wrapper/NodeWrapper';
+import NoteWrapper from '../Wrapper/NoteWrapper';
+
+
+export default function TextCutting({ data }: any) {
+
+    const text = 'Hover me to --------a-----!'
+    const delay = 100
+    const duration = 500
+
+    return (
+        <NodeWrapper heading='Text Cutting' description='Cutting text' setMaxW={true}>
+
+
+            <Box role="group" w='full'>
+                {/* Up : difference in transform, clipPath*/}
+                <Box position={'absolute'}>
+
+                    {text.split(" ").map((word, index) => (
+                        <Text as='span' fontSize='2xl' key={index} display='inline-block' m='0vmin 0.3vmin' overflow={'hidden'} clipPath={'polygon(0 50%, 100% 50%, 100% 100%, 0 100%)'}
+                            _groupHover={{ transform: 'translateY(40%)', opacity: '1', transition: `${duration}ms`, transitionDelay: `${index * delay}ms` }}
+                        >
+                            {word}
+                        </Text>
+                    ))}
+                </Box>
+
+                {/* Down : difference in transform, clipPath*/}
+                <Box position={'absolute'}>
+                    {text.split(" ").map((word, index) => (
+
+                        <Text as='span' fontSize='2xl' key={index} display='inline-block' m='0vmin 0.3vmin' overflow={'hidden'} clipPath={'polygon(0 0, 100% 0%, 100% 50%, 0 50%)'}
+                            _groupHover={{ transform: 'translateY(-40%)', opacity: '1', transition: `${duration}ms`, transitionDelay: `${index * delay}ms` }}
+                        >
+                            {word}
+                        </Text>
+                    ))}
+                </Box>
+
+                {/* Cut Indicator: use After*/}
+                <Box position={'absolute'}
+                    _after={{
+                        content: '""', w: '0', h: '5px', bg: 'red.200', opacity: '0.8',
+                        position: 'absolute', left: '0', top: '50%',
+
+                    }}
+                    _groupHover={{ _after: { opacity: '1', w: 'full', transition: `${delay * (text.split(" ").length)}ms` } }}
+                >
+
+                    {text.split(" ").map((word, index) => (
+                        <Text as='span' fontSize='2xl' key={index} display='inline-block' m='0vmin 0.3vmin' opacity={0}>
+                            {word}
+                        </Text>
+                    ))}
+                </Box>
+
+                <Text opacity={0} display='inline-block' m='0vmin 0.3vmin'>{text}</Text>
+            </Box>
+
+
+            <NoteWrapper>
+                <UnorderedList>
+                    <ListItem>
+                        Compose of two identical text, when hover: one moves up, another moves down,
+                    </ListItem>
+                    <ListItem>
+                        Use overflow:hidden and clip path to hide half of the text
+                    </ListItem>
+
+                    <ListItem>
+                        The cut is using the after attribute in css, so the length matches 
+                    </ListItem>
+
+                    <ListItem>
+                        Inspiration from: https://codepen.io/hrshainik/pen/mdVBwwm?editors=0100
+                    </ListItem>
+                </UnorderedList>
+            </NoteWrapper>
+        </NodeWrapper>
+    );
+}
--- data/nodeData.tsx
+++ data/nodeData.tsx
@@ -12,46 +12,60 @@ import ChakraProgress from '../components/Chakra/Chakra_Feedback/Chakra_Progress
 import ChakraSkeleton from '../components/Chakra/Chakra_Feedback/Chakra_Skeleton';
 import ChakraToast from '../components/Chakra/Chakra_Feedback/Chakra_Toast';
 import DragHandleNode from '../components/React-Flow/DragHandle';
+import TextCutting from '../components/Animation/TextCutting';
 
 
 export const initialEdges = [
-    { id: 'eAccordion-0', source: '5', target: '6' },
-    { id: 'eTab-0', source: '4', target: '7' },
-    { id: 'eTab-1', source: '4', target: '8' },
-    { id: 'eTab-2', source: '4', target: '9' },
+    // { id: 'eAccordion-0', source: '5', target: '6' },
+    // { id: 'eTab-0', source: '4', target: '7' },
+    // { id: 'eTab-1', source: '4', target: '8' },
+    // { id: 'eTab-2', source: '4', target: '9' },
 ];
 
 export const initialNodes: Node[] = [
     // { id: '1', type: 'textUpdater', position: { x: 0, y: 0 }, data: { value: 'blue.100' } },
     // { id: '2', type: 'textUpdater', position: { x: 500, y: 500 }, data: { value: 'green.100' } },
     { id: '3', type: 'TextAnimationNode', position: { x: 0, y: -1000 }, data: { value: 'green.100' } },
-    { id: '4', type: 'input', position: { x: -2400, y: 0 }, data: { label: 'Tab' } },
-    { id: '5', type: 'input', position: { x: -2400, y: 1000 }, data: { label: 'Accordion' } },
+    // { id: '4', type: 'input', position: { x: -2400, y: 0 }, data: { label: 'Tab' } },
+    // { id: '5', type: 'input', position: { x: -2400, y: 1000 }, data: { label: 'Accordion' } },
 
     {
         id: 'Disclosure',
         data: { label: 'Chakra Disclosure' },
-        position: { x: 320, y: 200 },
+        position: { x: 3000, y: 200 },
         className: 'light',
         style: { backgroundColor: 'rgba(255, 0, 0, 0.2)', width: 300, height: 300 },
     },
 
-    { id: '6', type: 'ChakraAccordion', position: { x: -0, y: 0 }, data: { value: 'green.100' }, parentNode: 'Disclosure', expandParent:true },
+    { id: '6', type: 'ChakraAccordion', position: { x: 0, y: 0 }, data: { value: 'green.100' }, parentNode: 'Disclosure', expandParent:true },
     { id: '7', type: 'ChakraTab', position: { x: 0, y: 600 }, data: { value: '' }, parentNode: 'Disclosure', expandParent:true},
 
-    { id: '10', type: 'ChakraAlert', position: { x: 1800, y: 0 }, data: { value: '' } },
-    { id: '11', type: 'ChakraCircularProgress', position: { x: 1800, y: 600 }, data: { value: '' } },
-    { id: '12', type: 'ChakraProgress', position: { x: 1800, y: 1200 }, data: { value: '' } },
-    { id: '13', type: 'ChakraSkeleton', position: { x: 1800, y: 1800 }, data: { value: '' } },
-    { id: '14', type: 'ChakraToast', position: { x: 1800, y: 2400 }, data: { value: '' } },
+    {
+        id: 'Feedback',
+        data: { label: 'Chakra Feedback' },
+        position: { x: 1800, y: 0 },
+        className: 'light',
+        style: { backgroundColor: 'rgba(255, 0, 0, 0.2)', width: 300, height: 300 },
+    },
+
+    { id: '10', type: 'ChakraAlert', position: { x: 0, y: 0 }, data: { value: '' }, parentNode: 'Feedback', expandParent:true  },
+    { id: '11', type: 'ChakraCircularProgress', position: { x: 0, y: 600 }, data: { value: '' }, parentNode: 'Feedback', expandParent:true },
+    { id: '12', type: 'ChakraProgress', position: { x: 0, y: 1200 }, data: { value: '' }, parentNode: 'Feedback', expandParent:true },
+    { id: '13', type: 'ChakraSkeleton', position: { x: 0, y: 1800 }, data: { value: '' }, parentNode: 'Feedback', expandParent:true },
+    { id: '14', type: 'ChakraToast', position: { x: 0, y: 2400 }, data: { value: '' }, parentNode: 'Feedback', expandParent:true },
 
     // React flow
     { id: '15', type: 'DragHandleNode', position: { x: -3000, y: 0 }, dragHandle: '.custom-drag-handle', data: { value: '' } },
+
+    //Animation
+    { id: '100', type: 'TextCutting', position: { x: 0, y: 0 },  data: { value: '' } },
+
 ];
 
 export const nodeTypes = {
     TextUpdaterNode,
     TextAnimationNode,
+    TextCutting,
 
     ChakraAccordion,
     ChakraTab,
