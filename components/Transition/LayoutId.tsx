4b63bea825173dfa6e249251ca22a5cd9423fecc
jasonhui1 <jason903813@gmail.com>
2023-04-17 19:58:53 +0800

    add swap list example

--- components/Transition/LayoutId.tsx
+++ components/Transition/LayoutId.tsx
@@ -4,25 +4,18 @@ import { AnimatePresence, motion } from 'framer-motion';
 import NodeWrapper from "../Wrapper/NodeWrapper";
 import NoteWrapper from "../Wrapper/NoteWrapper";
 
-export default function LayoutIdExample() {
-    const [selecetedIndex, setSelecetedIndex] = useState(1)
 
+
+
+export default function LayoutIdExample() {
 
     return (
         <NodeWrapper heading={"Layout Id Example"} description={"swap list item using example"} setMaxW={true}>
-            <Stack>
-                <BoxWithRandomTranslationX width={100} translateX={100} underline={selecetedIndex === 0} />
-                <BoxWithRandomTranslationX width={150} translateX={400} underline={selecetedIndex === 1} />
-                <BoxWithRandomTranslationX width={120} translateX={200} underline={selecetedIndex === 2} />
+
+            <Stack gap={5}>
+                <Example1 />
+                <Example2 />
             </Stack>
-
-            <Flex justify={'space-around'} mt='5' gap='5'>
-
-                {[0, 1, 2].map(index => (
-                    <Button flexGrow={1} onClick={() => setSelecetedIndex(index)} key={index} colorScheme={selecetedIndex === index ? "green" : "blue"}>{index} </Button>
-                ))}
-            </Flex>
-
 
             <NoteWrapper>
                 <UnorderedList>
@@ -46,6 +39,8 @@ export default function LayoutIdExample() {
     )
 };
 
+
+
 interface Props {
     width: number
     translateX: number
@@ -62,3 +57,76 @@ function BoxWithRandomTranslationX({ width, translateX, underline = false }: Pro
         </>
     )
 }
+
+
+
+function Example1() {
+    const [selecetedIndex, setSelecetedIndex] = useState(1)
+
+    return (
+        <>
+            <Stack>
+                <BoxWithRandomTranslationX width={100} translateX={100} underline={selecetedIndex === 0} />
+                <BoxWithRandomTranslationX width={150} translateX={400} underline={selecetedIndex === 1} />
+                <BoxWithRandomTranslationX width={120} translateX={200} underline={selecetedIndex === 2} />
+            </Stack>
+
+            <Flex justify={'space-around'} mt='5' gap='5'>
+
+                {[0, 1, 2].map(index => (
+                    <Button flexGrow={1} onClick={() => setSelecetedIndex(index)} key={index} colorScheme={selecetedIndex === index ? "green" : "blue"}>{index} </Button>
+                ))}
+            </Flex>
+        </>
+    )
+}
+
+const startItem = [
+    { id: '1', name: 'banana' },
+    { id: '2', name: 'apple' },
+    { id: '3', name: 'orange' },
+    { id: '4', name: 'pear' },
+    { id: '5', name: 'strawberry' },
+];
+
+
+function Example2() {
+    const [items, setItems] = useState(startItem)
+
+    const onSwap = () => {
+        setItems(items => {
+            let updateItems = [...items]
+            const temp = updateItems[0];
+            updateItems[0] = updateItems[3];
+            updateItems[3] = temp;
+            return updateItems
+        })
+    }
+
+    return (
+        <Flex gap='5'>
+            <Stack>
+                {items.map((item) => (
+                    <motion.div
+                        key={item.id}
+                        layoutId={item.id}
+                        initial={{ opacity: 0, maxHeight: 0 }}
+                        animate={{ opacity: 1, maxHeight: '50px' }}
+                        exit={{ opacity: 0, maxHeight: 0 }}
+                        transition={{ duration: 1 }}
+                    >
+                        <Box w='200px' bg='blue.200' key={item.id} p='4' rounded='3xl' >
+                            {item.name}
+                        </Box>
+                    </motion.div>
+                ))}
+
+            </Stack >
+
+            <Box>
+                <Button onClick={() => onSwap()}>Swap item</Button>
+            </Box>
+        </Flex>
+    )
+}
+
